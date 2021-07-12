1ed6893e0ac925b6d95d784241bf0b496f8704fa
TobiOnabolu <toby.onabolu@gmail.com>
2021-07-12 14:35:59 -0400

    Preparing work space

---
+++ lechic-laces-website/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navabar.css'
+
+
+function Navbar() {
+    return (
+        <div>
+            
+        </div>
+
+)
+}
+
+export default Navbar
---
+++ lechic-laces-website/src/Components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import './Home.css'
+
+function Home() {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
+
+
+export default Home
